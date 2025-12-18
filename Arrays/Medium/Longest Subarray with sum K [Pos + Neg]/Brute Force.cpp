c8754986a7b4c6064ed91484485030710770c3f3
Aditya Pateriya <adityapateriya7986@gmail.com>
2025-12-18 20:08:46 +0530

    Longest Subarray with sum K

---
+++ Arrays/Medium/Longest Subarray with sum K [Pos + Neg]/Brute Force.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int longestSubarray(vector<int>& nums, int k) {
+    int n = nums.size();
+
+    int maxLength = 0;
+    for(int i=0; i<n; i++) {
+        int sum = 0;
+        for(int j=i; j<n; j++) {
+            sum += nums[j];
+            if(sum == k) {
+                maxLength = max(maxLength, j-i+1);
+            }
+        }
+    }
+
+    return maxLength ? maxLength : 0;
+}
+
+int main() {
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    int n;
+    cin >> n;
+
+    vector<int> nums(n);
+
+    for(int i=0; i<n; i++) {
+        cin >> nums[i];
+    }
+
+    int k;
+    cin >> k;
+
+    cout << "The longest subarray is of length " << longestSubarray(nums, k);
+
+    return 0;
+}
