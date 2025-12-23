ab8988aeacfcebb87374a0dc5e3be14b401a98ea
Aditya Pateriya <adityapateriya7986@gmail.com>
2025-12-23 11:36:15 +0530

    Better approach.cpp

---
+++ Arrays/Medium/Longest Subarray with sum K [Pos + Neg]/Better approach.cpp
@@ -0,0 +1,52 @@
+// TC = O(nlogn)
+// SC = O(n)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int longestSubarray(vector<int>& nums, int k) {
+    int n = nums.size();
+
+    map<int, int> mpp;
+
+    int prefixSum = 0;
+    int maxLen = 0;
+    for(int i=0 ;i<n; i++) {
+        prefixSum += nums[i];
+        
+        if(prefixSum == k)
+            maxLen = max(maxLen, i+1);
+
+        if(mpp.find(prefixSum-k) != mpp.end()) {
+            maxLen = max(maxLen, i - mpp[prefixSum-k]);
+        }
+        if(mpp.find(prefixSum) == mpp.end()) {
+            mpp[prefixSum] = i;
+        }
+    } 
+
+    return maxLen;    
+}
+
+int main() {
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    int n;
+    cin >> n;
+
+    vector<int> nums(n);
+
+    for(int i=0; i<n; i++) {
+        cin >> nums[i];
+    }
+
+    int k;
+    cin >> k;
+
+    cout << "The longest subarray is of length " << longestSubarray(nums, k);
+
+    return 0;
+}
